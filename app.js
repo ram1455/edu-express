982e8390d68a2302ef7f0754d73fb5208a1b24d7
ram1455 <puterateguh777@gmail.com>
2022-10-07 17:39:57 +0700

    "add upload file"

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express   = require('express')
 const app       = express()
 const router    = require('./routes')
+const path      = require('path')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use('/public',express.static(path.join(__dirname, 'uploads')))
 app.use(router)
 app.use((req,res)=>{
     res.status(404)
--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const multer = require('multer')
 const upload = multer({dest: 'uploads'}) 
+const fs     = require('fs')
+const path   = require('path')
 
 // html from another local file
 router.get('/', (req ,res)=>{
@@ -30,7 +32,7 @@ router.get('/about', (req ,res)=>{
         halamanlain : '/about atau /' 
     })
 })
-
+ 
 
 // param json
 router.get('/product/:id/:jenis', (req ,res)=>{
@@ -41,16 +43,22 @@ router.get('/product/:id/:jenis', (req ,res)=>{
     })
 })
 
-router.post('/product',upload.single('image'), (req ,res)=>{    
+router.post('/product/',upload.single('image'), (req ,res)=>{    
     const {name,price,stock,status} = req.body
-    const image= req.files  
-    res.json({
-        name,
-        price,
-        stock,
-        status,
-        image
-    })
+    const image = req.file 
+    if (image) {
+        const target = path.join(__dirname, 'uploads', image.originalname)
+        fs.renameSync(image.path, target)
+
+        // res.json({
+        //     name,
+        //     price,
+        //     stock,
+        //     status,
+        //     image
+        // })
+        res.sendFile(target)
+    }
 })
 
 module.exports = router;
